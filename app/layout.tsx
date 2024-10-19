fec19b1f85440cd0c581df166ca619b6f8cff09b
しまぶー <0120koto@gmail.com>
2024-10-19 16:47:23 +0900

    ⬆️

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
       <body>{children}</body>
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,6 @@ import type { NextConfig } from 'next'
 export default {
   experimental: {
     ppr: true,
-    dynamicIO: true,
+    reactCompiler: true,
   },
 } satisfies NextConfig
