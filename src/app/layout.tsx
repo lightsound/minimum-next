24d23b0df24254996acde933d6b799730aa1b83e
しまぶー <0120koto@gmail.com>
2024-12-15 15:47:08 +0900

    ⬆️

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { NextLayoutProps } from '~/app/type'
-import '~/css/globals.css'
+import '~/css/app.css'
 
 export default function RootLayout({ children }: NextLayoutProps) {
   return (
-    <html lang="ja">
-      <body>{children}</body>
+    <html lang="ja" suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/type.ts
+++ src/app/type.ts
@@ -1,13 +1,17 @@
 type NonEmptyObject = Record<string, unknown> & {}
 
-export interface NextLayoutProps<Params = never> {
+type ExpandKeys<T extends string> = T extends never
+  ? object
+  : { [K in T]: React.ReactNode }
+
+export type NextLayoutProps<Params = undefined, Keys extends string | undefined = undefined> = {
   children: React.ReactNode
   params: Params extends NonEmptyObject ? Promise<Params> : never
-}
+} & (Keys extends string ? ExpandKeys<Keys> : object)
 
 export interface NextPageProps<
   Params extends NonEmptyObject | undefined,
-  SearchParams extends NonEmptyObject | undefined,
+  SearchParams extends NonEmptyObject | undefined = undefined,
 > {
   params: Params extends NonEmptyObject ? Promise<Params> : never
   searchParams: SearchParams extends NonEmptyObject ? Promise<SearchParams> : never
