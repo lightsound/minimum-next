63da437b934d35a2f5ca98b660e4d788674425ca
しまぶー <0120koto@gmail.com>
2024-12-29 11:42:17 +0900

    ⬆️

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,13 +8,21 @@ import tailwind from 'eslint-plugin-tailwindcss'
 const compat = new FlatCompat({ baseDirectory: path.dirname(fileURLToPath(import.meta.url)) })
 
 export default antfu(
-  { react: true, ignores: [] },
+  { react: true, ignores: ['src/type/auth.d.ts'] },
   {
     name: 'react-compiler/recommended',
     plugins: { 'react-compiler': reactCompiler },
     rules: { 'react-compiler/react-compiler': 'error' },
   },
   ...compat.extends('plugin:@next/next/core-web-vitals'),
+  // Tailwind CSS
   ...tailwind.configs['flat/recommended'],
-  { settings: { tailwindcss: { callees: ['cn'] } } },
+  { settings: { tailwindcss: { callees: ['cx'] } } },
+  // https://github.com/sweepline/eslint-plugin-unused-imports/
+  {
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': ['warn', { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }],
+    },
+  },
 )
